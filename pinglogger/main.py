44f617387232e9dc46aab6ca843d578533b8d702
Jared Claypoole <jared.claypoole@gmail.com>
2022-03-28 21:11:30 -0400

    Allow url to be changed via command line argument

--- pinglogger/main.py
+++ pinglogger/main.py
@@ -1,7 +1,10 @@
 import os
+import argparse
 
-def main():
-    url = "google.com"
-    cmd = f"ping -t {url}"
+def main(cmdline_args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", type=str, default="google.com", help="The url to ping")
+    args = parser.parse_args(cmdline_args)
 
+    cmd = f"ping -t {args.url}"
     os.system(cmd)
