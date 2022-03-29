d9f0b9cfaf8ef7e83671c575e3415477348d94aa
Jared Claypoole <jared.claypoole@gmail.com>
2022-03-28 22:20:18 -0400

    Prepend a timestamp to each line of ping output

--- pinglogger/main.py
+++ pinglogger/main.py
@@ -1,6 +1,8 @@
 import subprocess
 import argparse
 
+from .prependtimestamp import prepend_timestamp
+
 def main(cmdline_args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("--url", type=str, default="google.com", help="The url to ping")
@@ -14,7 +16,15 @@ def main(cmdline_args=None):
     try:
         proc = subprocess.Popen(
             cmd_tokens,
+            stdout=subprocess.PIPE,
         )
+
+        for line in proc.stdout:
+            line = line.decode()
+            line = line.strip()
+            line = prepend_timestamp(line)
+            print(line)
+
         proc.communicate()
 
     except KeyboardInterrupt:
